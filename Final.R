2a29e4eac0c227de7d72fc83a3ccadee81b94315
krissymchugh <krissy161.8@gmail.com>
2021-12-07 16:53:47 -0500

    loaded tidyverse and csv files

--- Final.R
+++ Final.R
@@ -4,3 +4,12 @@
 # Directory: /Users/krissymchugh/Desktop/STA308/Take-Home-Final
 #
 ###############################################################
+#
+library(tidyverse)
+death_numbers <- read_csv("state_Opioid_Deaths2019.csv")
+us_states_mapping <- read_csv("state_abb_codes.csv")
+census_mapping <- read_csv("censusRegions.csv")
+
+view(death_numbers)
+view(us_states_mapping)
+view(census_mapping)

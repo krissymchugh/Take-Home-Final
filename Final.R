ace20dd876e6b2b1e2d43e60e0fbcdbad6b4c1c2
krissymchugh <krissy161.8@gmail.com>
2021-12-07 16:38:05 -0500

    added heading

---
+++ Final.R
@@ -0,0 +1,6 @@
+# Final
+# Author: Krissy McHugh
+# Date: 12/7/2021
+# Directory: /Users/krissymchugh/Desktop/STA308/Take-Home-Final
+#
+###############################################################
